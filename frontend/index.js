0fd386a9c7a009ca0b4bd1dc1df10dafa4260ca4
AlaaSayed794 <alaa.elsayed.aly@gmail.com>
2022-08-21 21:31:29 +0200

    fix index.js frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,12 @@
+window.onload = fetchData
 let i = 0
+
 function btnFunc() {
     i++
     console.log("clicked " + i + " times")
     document.getElementById('paragraph').innerText = "clicked " + i + " times"
 }
 
-fetch("http://newapp-env.eba-2nyd3gg8.us-east-1.elasticbeanstalk.com/").then(res => res.json()).then(data => document.getElementById('data').innerText = data)
+function fetchData() {
+    fetch("http://newapp-env.eba-2nyd3gg8.us-east-1.elasticbeanstalk.com/").then(res => res.json()).then(data => document.getElementById('data').innerText = data.message)
+}
