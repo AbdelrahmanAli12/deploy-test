d6060f661b945368028b666b7db9ae5945935f8e
AlaaSayed794 <alaa.elsayed.aly@gmail.com>
2022-08-21 21:35:36 +0200

    remove udacity line

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -1,2 +1,1 @@
 aws s3 cp --recursive --acl public-read . s3://deploytestalaasayed1/ --region us-east-1
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./index.html s3://deploytestalaasayed1/
